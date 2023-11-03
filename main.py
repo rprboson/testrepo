457882170d59fcbdcbf640bc3879f7124a9b22b6
rprboson <145299464+rprboson@users.noreply.github.com>
2023-11-03 14:14:52 -0300

    Update main.py
    Apaguei comentário

--- main.py
+++ main.py
@@ -2,11 +2,7 @@ import requests
 from bs4 import BeautifulSoup
 from googlesearch import search
 import xml.etree.ElementTree as ET
-"""
-query="imoveis humaira"
-for url in search(query, sleep_interval=5, num_results=200):
-    print(url)
-"""
+
 file='RJS739-NW3_BlackOil.mr2'
 tree=ET.parse(file)
 root=tree.getroot()
